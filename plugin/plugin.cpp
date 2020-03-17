85166f4bb0ea8359d9da3d1d48d9a31114f69194
Manuel Odendahl <wesen@ruinwesen.com>
2020-03-17 19:57:30 -0400

    :ambulance: Somehow this seems to work

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -1,4 +1,5 @@
 #include "plugin.hpp"
+#include "MyModule.h"
 
 
 Plugin* pluginInstance;
@@ -8,7 +9,8 @@ void init(Plugin* p) {
 	pluginInstance = p;
 
 	// Add modules here
-	// p->addModel(modelMyModule);
+    Model *const model = createModel<MyModule, MyModuleWidget>("MyModule");
+    p->addModel(model);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
